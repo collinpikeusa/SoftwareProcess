f3426a4aadd3a22cb10153adcb166f9691bbcec8
Collin Pike <collinpikeusa@auburn.edu>
2018-09-22 15:39:32 -0500

    Red Light |

--- RCube/test/createCubeTest.py
+++ RCube/test/createCubeTest.py
@@ -30,9 +30,41 @@ class CreateCubeTest(unittest.TestCase):
 #            'red', 'red', 'red',
 #            'orange', 'orange', 'orange',
 #            'orange', 'orange', 'orange', 'orange', 'orange', 'orange']} 
+#
+#    test 020
+#    input:    parm: {'op': 'create'}    no options
+#    output:    default model cube, which is a JSON string:
+#        {'status': 'created', 'cube': [
+#            'green', 'green', 'green',
+#            'green', 'green', 'green',
+#            'green', 'green', 'green', 
+#            'yellow', 'yellow', 'yellow',
+#            'yellow', 'yellow', 'yellow',
+#            'yellow', 'yellow', 'yellow',
+#            'blue', 'blue', 'blue',
+#            'blue', 'blue', 'blue',
+#            'blue', 'blue', 'blue',
+#            'white', 'white', 'white',
+#            'white', 'white', 'white',
+#            'white', 'white', 'white',
+#            'red', 'red', 'red',
+#            'red','red', 'red',
+#            'red', 'red', 'red',
+#            'orange', 'orange', 'orange',
+#            'orange', 'orange', 'orange', 'orange', 'orange', 'orange']}
 
     def test100_010_ShouldCreateEntireDefaultCube(self):
         parm = {'op': 'create'}
+        expectedFaces = ['green', 'yellow', 'blue', 'white', 'red', 'orange']
+        actualResult = RCube.createCube(parm)
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+    
+    def test100_010_ShouldCreateCubeWithOneDifferentSide(self):
+        parm = {'op': 'create', 'f': 'purple'}
         expectedFaces = ['green', 'yellow', 'blue', 'white', 'red', 'orange']
         actualResult = RCube.createCube(parm)
         elementIndex = 0
