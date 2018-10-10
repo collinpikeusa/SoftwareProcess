2eb960fdc3eedfcd2511e14f89205099159dac12
Collin Pike <collinpikeusa@auburn.edu>
2018-10-10 16:41:26 -0500

    Refactor |

--- RCube/dispatch.py
+++ RCube/dispatch.py
@@ -12,17 +12,6 @@ def dispatch(parm={}):
     if(not('op' in parm)):
         httpResponse['status'] = 'error: missing op'                
     elif(parm['op'] == 'create'):
-        duplicate = 0
-        for i in parm.values():
-            for j in parm.values():
-                if(i == j):
-                    duplicate += 1
-            if(duplicate > 1):
-                break
-            else:
-                duplicate = 0
-        if(duplicate > 1):
-            httpResponse['status'] = 'error: at least two faces have the same color'
         cube = createCube(parm)
         if(isinstance(cube, int)):
             httpResponse['status'] = 'error: at least two faces have the same color'
@@ -33,40 +22,46 @@ def dispatch(parm={}):
 
 
 # ------------------- Inner Functions ---------------------
+
+def isDuplicate(colors):
+    duplicate = 0
+    for i in colors:
+        for j in colors:
+            if (i == j):
+                duplicate += 1
+        
+        if (duplicate > 1):
+            cube = -1
+        else:
+            duplicate = 0
+    
+    return cube
+
 def createCube(parm):
     cube = []
     colors = ['green', 'yellow', 'blue', 'white', 'red', 'orange']
     if('f' in parm):
         if(parm['f'] is not ''):
-            colors[0] = parm['f'].lower()
+            colors[0] = parm['f']
     if('r' in parm):
         if(parm['f'] is not ''):
-            colors[1] = parm['r'].lower()
+            colors[1] = parm['r']
     if('b' in parm):
         if(parm['f'] is not ''):
-            colors[2] = parm['b'].lower()
+            colors[2] = parm['b']
     if('l' in parm):
         if(parm['f'] is not ''):
-            colors[3] = parm['l'].lower()
+            colors[3] = parm['l']
     if('t' in parm):
         if(parm['f'] is not ''):
-            colors[4] = parm['t'].lower()
+            colors[4] = parm['t']
     if('u' in parm):
         if(parm['f'] is not ''):
-            colors[5] = parm['u'].lower()
+            colors[5] = parm['u']
     for face in colors:
         cube.extend(createSide(face))
 
-    duplicate = 0
-    for i in colors:
-        for j in colors:
-            if(i == j):
-                duplicate += 1
-        if(duplicate > 1):
-            cube = -1
-            break
-        else:
-            duplicate = 0
+    cube = isDuplicate(colors)
     return cube
 
 def createSide(parm):
--- RCube/test/dispatchTest.py
+++ RCube/test/dispatchTest.py
@@ -1,9 +1,3 @@
-'''
-    Functions used for dispatch of microservice
-    Created on Sep 22, 2018
-    
-    @author: Collin Pike
-'''
 import unittest
 import httplib
 import json
@@ -64,16 +58,13 @@ class DispatchTest(unittest.TestCase):
 # outputs:    A JSON string containing, at a minimum, a key of "status"
 #
 # Happy path 
-#    test 010
 #      input:   parm having at least one element with a key of "op"        
-#      output:  JSON string containing a key of "status"
-#      
+#      output:  JSON string containing a key of "status" 
+#
 # Sad path 
-#    test 910
 #      input:   no string       
 #      output:  dictionary consisting of an element with a key of "status" and value of "error: missing op"
 #
-#    test 920
 #      input:   valid parm string with at least one key-value pair, no key of "op"
 #      output:  dictionary consisting of an element with a key of "status" and value of "error: missing op"
 #
@@ -89,8 +80,7 @@ class DispatchTest(unittest.TestCase):
         resultDict = self.string2dict(resultString)
         self.assertIn('status', resultDict)
     
-    # Sad path
-    
+# Sad path
     def test100_900_ShouldReturnErrorOnEmptyParm(self):
         queryString=""
         resultString = self.httpGetAndResponse(queryString)
@@ -105,111 +95,290 @@ class DispatchTest(unittest.TestCase):
         self.assertIn('status', resultDict)
         self.assertEquals('error:',resultDict['status'][0:6])
     
-# Acceptance Test
-# 200 Dispatch -op=create
-# Confidence Level: BVA
-# Analysis
-#      inputs:     http://url/rcube?op=create<options>
-#                        where options can be zero or one of the following:
-#                            f=<string>    String of length GT 0. Optional. Default to "green". Unvalidated
-#                            r=<string>    String of length GT 0. Optional. Default to "yellow". Unvalidated        
-#                            b=<string>    String of length GT 0. Optional. Default to "blue". Unvalidated        
-#                            l=<string>    String of length GT 0. Optional. Default to "white". Unvalidated        
-#                            t=<string>    String of length GT 0. Optional. Default to "red". Unvalidated        
-#                            u=<string>    String of length GT 0. Optional. Default to "orange". Unvalidated   
-#      outputs:     A JSON string containing, at a minimum, a key of "status"
+#     def test100_920_ShouldReturnErrorOnInvalidOp(self):
+#         queryString = "op=initiate"
+#         resultString = self.httpGetAndResponse(queryString)
+#         resultDict = self.string2dict(resultString)
+#         self.assertIn('status', resultDict)
+#         self.assertEquals('error:', resultDict['status'][0:6])
+#         
+#Acceptance Tests
 #
-# Happy path:
-#    input:     zero options
-#        http://url/rcube?op=create
-#    output:    default model cube, which is a JSON string:
-#        {'status': 'created', 'cube': [
-#            'green', 'green', 'green',
-#            'green', 'green', 'green',
-#            'green', 'green', 'green', 
-#            'yellow', 'yellow', 'yellow',
-#            'yellow', 'yellow', 'yellow',
-#            'yellow', 'yellow', 'yellow',
-#            'blue', 'blue', 'blue',
-#            'blue', 'blue', 'blue',
-#            'blue', 'blue', 'blue',
-#            'white', 'white', 'white',
-#            'white', 'white', 'white',
-#            'white', 'white', 'white',
-#            'red', 'red', 'red',
-#            'red','red', 'red',
-#            'red', 'red', 'red',
-#            'orange', 'orange', 'orange',
-#            'orange', 'orange', 'orange',
-#            'orange', 'orange', 'orange']}  
+# 200 dispatch -- op=create
+# Desired level of confidence: boundary value analysis
+# Analysis 
+# inputs:    http:// ...myURL... /rcube?op=create<options>
+#            where <options> can be zero or one of:
+#                    "f"    Specifies the color of the front side of the cube.  It is a string of length .GT. 0.  Optional.  Defaults to "green" if missing.  Arrives unvalidated.        
+#                    "r"    Specifies the color of the right side of the cube.  It is a string of length .GT. 0.  Optional.  Defaults to "yellow" if missing.  Arrives unvalidated.        
+#                    "b"    Specifies the color of the back side of the cube.  It is a string of length .GT. 0.  Optional.  Defaults to "blue" if missing.  Arrives unvalidated.        
+#                    "l"    Specifies the color of the left side of the cube.  It is a string of length .GT. 0.  Optional.  Defaults to "white" if missing.  Arrives unvalidated.        
+#                    "t"    Specifies the color of the top side of the cube.  It is a string of length .GT. 0.  Optional.  Defaults to "red" if missing.  Arrives unvalidated.        
+#                    "u"    Specifies the color of the under side of the cube.  It is a string of length .GT. 0.  Optional.  Defaults to "orange" if missing.  Arrives unvalidated.        
 #
-#    input:     empty option value
-#        http://url/rcube?op=create&f=
-#    output:    default model cube, which is a JSON string:
-#        {'status': 'created', 'cube': [
-#            'green', 'green', 'green',
-#            'green', 'green', 'green',
-#            'green', 'green', 'green', 
-#            'yellow', 'yellow', 'yellow',
-#            'yellow', 'yellow', 'yellow',
-#            'yellow', 'yellow', 'yellow',
-#            'blue', 'blue', 'blue',
-#            'blue', 'blue', 'blue',
-#            'blue', 'blue', 'blue',
-#            'white', 'white', 'white',
-#            'white', 'white', 'white',
-#            'white', 'white', 'white',
-#            'red', 'red', 'red',
-#            'red','red', 'red',
-#            'red', 'red', 'red',
-#            'orange', 'orange', 'orange',
-#            'orange', 'orange', 'orange',
-#            'orange', 'orange', 'orange']} 
+# outputs:    A JSON string containing, at a minimum, a key of "status"
 #
-# Sad Path
-#    test 910
-#        input: http://url/rcube?op=create&f=red&u=red
-#        output: {'status': 'error: at least two faces have the same color'}
-#    test 920
-#        input: http:// ... /rcube?op=create&f=f&r=r&b=b&l=l&t=1&u=0
-#        output {'status': 'error: at least one face had a negative value'}
-       
+# Happy path 
+#      input:   zero options
+#               http:// ... myURL ... /rcube?op=create
+#      output:  default model cube, which is JSON string: 
+#                {'status': 'created', 'cube': [
+#                  'green',  'green', 'green', 
+#                  'green', 'green', 'green',
+#                  'green', 'green', 'green',
+#                  'yellow', 'yellow', 'yellow', 
+#                  'yellow', 'yellow', 'yellow',
+#                  'yellow', 'yellow', 'yellow',  
+#                  'blue', 'blue', 'blue',
+#                  'blue', 'blue', 'blue', 
+#                  'blue', 'blue', 'blue', 
+#                  'white', 'white', 'white', 
+#                  'white', 'white', 'white',
+#                  'white', 'white', 'white',
+#                  'red', 'red', 'red',
+#                  'red', 'red', 'red', 
+#                  'red', 'red', 'red',
+#                  'orange', 'orange', 'orange',
+#                  'orange', 'orange', 'orange', 
+#                  'orange', 'orange', 'orange']}        
 
+# Happy path
     def test200_010_ShouldCreateDefaultCubeStatus(self):
         queryString="op=create"
         resultString = self.httpGetAndResponse(queryString)
         resultDict = self.string2dict(resultString)
         self.assertIn('status', resultDict)
         self.assertEquals('created', resultDict['status'][0:7])
+     
     
-    def test200_020_ShouldCreateDefaultCubeKey(self):
+    def test200_020ShouldCreateDefaultCubeKey(self):
         queryString="op=create"
         resultString = self.httpGetAndResponse(queryString)
         resultDict = self.string2dict(resultString)
         self.assertIn('cube', resultDict)
+
+    def test200_030ShouldCreateDefaultCubeList(self):
+        queryString="op=create"
+        expectedFaces = ['green', 'yellow', 'blue', 'white', 'red', 'orange']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube']   
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
     
-    def test200_030_ShouldCreateDefaultCube(self):
-        queryString="op=create&f="
+    def test200_040_ShouldCreateMultipleFaceCubeWithOneFaceOnInputKey(self):
+        queryString="op=create&f=f"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('cube', resultDict)
+
+
+    def test200_050_ShouldCreateMultipleFaceCubeWithOneFaceOnInputStatus(self):
+        queryString="op=create&f=f"
         resultString = self.httpGetAndResponse(queryString)
         resultDict = self.string2dict(resultString)
         self.assertIn('status', resultDict)
         self.assertEquals('created', resultDict['status'][0:7])
         
-    def test200_910_ShouldReturnErrorDuplicateColors(self):
-        queryString="op=create&f=red&u=red"
+    def test200_060_ShouldCreateMultipleFaceCubeWithOneFaceOnInput(self):
+        queryString="op=create&f=f"
+        expectedFaces = ['f', 'yellow', 'blue', 'white', 'red', 'orange']
         resultString = self.httpGetAndResponse(queryString)
-        resultDict = self.string2dict(resultString)
-        self.assertIn('error', resultDict['status'][0:5])  
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
     
-    def test200_920_ShouldReturnErrorDuplicateColorsOneGiven(self):
-        queryString="op=create&f=yellow"
+    def test200_070_ShouldCreateMultipleFaceCubeWithTwoFacesOnInputKey(self):
+        queryString="op=create&f=f&r=2"
         resultString = self.httpGetAndResponse(queryString)
         resultDict = self.string2dict(resultString)
-        self.assertIn('error', resultDict['status'][0:5]) 
+        self.assertIn('cube', resultDict)
+
+    def test200_080_ShouldCreateMultipleFaceCubeWithTwoFacesOnInputStatus(self):
+        queryString="op=create&f=f&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('created', resultDict['status'][0:7])   
     
-    def test200_930_ShouldReturnErrorDuplicateColorsOneGivenUpperCase(self):
-        queryString="op=create&f=Yellow"
+    def test200_090_ShouldCreateMultipleFaceCubeWithTwoFacesOnInput(self):
+        queryString="op=create&f=f&r=2"
+        expectedFaces = ['f', '2', 'blue', 'white', 'red', 'orange']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+    
+    def test200_100_ShouldCreateMultipleFaceCubeWithThreeFacesOnInputKey(self):
+        queryString="op=create&b=b&f=f&r=2"
         resultString = self.httpGetAndResponse(queryString)
         resultDict = self.string2dict(resultString)
-        self.assertIn('error', resultDict['status'][0:5])  
+        self.assertIn('cube', resultDict)
+        
+    def test200_110_ShouldCreateMultipleFaceCubeWithThreeFacesOnInputStatus(self):
+        queryString="op=create&b=b&f=f&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('created', resultDict['status'][0:7])   
+           
+    def test200_120_ShouldCreateMultipleFaceCubeWithThreeFacesOnInput(self):
+        queryString="op=create&b=b&f=f&r=2"
+        expectedFaces = ['f', '2', 'b', 'white', 'red', 'orange']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+    
+    def test200_130_ShouldCreateMultipleFaceCubeWithFourFacesOnInputKey(self):
+        queryString="op=create&b=b&f=f&l=4&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('cube', resultDict)          
+    
+    def test200_140_ShouldCreateMultipleFaceCubeWithFourFacesOnInputStatus(self):
+        queryString="op=create&b=b&f=f&l=4&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('created', resultDict['status'][0:7])   
+                     
+    def test200_150_ShouldCreateMultipleFaceCubeWithFourFacesOnInput(self):
+        queryString="op=create&b=b&f=f&l=4&r=2"
+        expectedFaces = ['f', '2', 'b', '4', 'red', 'orange']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+    
+    def test200_160_ShouldCreateMultipleFaceCubeWithFiveFacesOnInputKey(self):
+        queryString="op=create&t=t&b=b&f=f&l=4&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('cube', resultDict) 
+        
+    def test200_170_ShouldCreateMultipleFaceCubeWithFiveFacesOnInputStatus(self):
+        queryString="op=create&t=t&b=b&f=f&l=4&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('created', resultDict['status'][0:7])   
+        
+    def test200_180_ShouldCreateMultipleFaceCubeWithFourFacesOnInput(self):
+        queryString="op=create&t=t&b=b&f=f&l=4&r=2"
+        expectedFaces = ['f', '2', 'b', '4', 't', 'orange']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+    
+    def test200_190_ShouldCreateMultipleFaceCubeWithSixFacesOnInputKey(self):
+        queryString="op=create&u=1&t=t&b=b&f=f&l=4&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('cube', resultDict) 
+    
+    def test200_200_ShouldCreateMultipleFaceCubeWithSixFacesOnInputKey(self):
+        queryString="op=create&u=1&t=t&b=b&f=f&l=4&r=2"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('created', resultDict['status'][0:7])   
+    
+    def test200_210_ShouldCreateMultipleFaceCubeWithSixFacesOnInput(self):
+        queryString="op=create&u=1&t=t&b=b&f=f&l=45&r=200"
+        expectedFaces = ['f', '200', 'b', '45', 't', '1']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+                
+    def test200_220_ShouldCreateMultipleFaceCubeWithSixFacesOnInput(self):
+        queryString="op=create&u=u&t=t&b=b&f=f&l=l&r=r"
+        expectedFaces = ['f', 'r', 'b', 'l', 't', 'u']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+                
+    def test200_230_ShouldCreateMultipleFaceCubeWithInvalidFacesOnInput(self):
+        queryString="op=create&u=u&t=t&b=b&f=f&l=l&right=r"
+        expectedFaces = ['f', 'yellow', 'b', 'l', 't', 'u']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+                
+    def test200_240_ShouldCreateMultipleFaceCubeWithInvalidFacesOnInput(self):
+        queryString="op=create&u=u&top=t&b=b&f=f&l=l&right=r"
+        expectedFaces = ['f', 'yellow', 'b', 'l', 'red', 'u']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+            
+    def test200_250_ShouldCreateMultipleFaceCubeWithMulitpleFacesWithCaseSensitiveColors(self):
+        queryString="op=create&u=red&t=Red&b=blue&f=Blue&l=white&r=White"
+        expectedFaces = ['Blue', 'White', 'blue', 'white', 'Red', 'red']
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)     
+        actualResult = resultDict['cube'] 
+        elementIndex = 0
+        for face in expectedFaces:
+            for _ in range(0,9):
+                self.assertEqual(face, actualResult[elementIndex])
+                elementIndex += 1
+# Sad path
+    def test200_900_ShouldReturnErrorOnSameColorInInput(self):
+        queryString="op=create&f=purple&l=purple"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('error:',resultDict['status'][0:6])
+     
+    def test200_910_ShouldReturnErrorOnSameOnOutput(self):
+        queryString ="op=create&f=1&b=yellow"
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('error:',resultDict['status'][0:6])
     
