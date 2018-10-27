4a5d7d7a2d4c5ad3f66e52e673acd50bfdb6e3f2
Collin Pike <collinpikeusa@auburn.edu>
2018-10-27 18:03:11 -0500

    Red Light |

--- RCube/test/dispatchTest.py
+++ RCube/test/dispatchTest.py
@@ -735,4 +735,11 @@ class DispatchTest(unittest.TestCase):
         self.assertIn('status', resultDict)
         self.assertEquals('rotated',resultDict['status'])
         self.assertEquals(expected, resultDict['cube'])
+    def test400_060_ShouldReturnRotatedCubeOnR(self):
+        queryString = "op=rotate&f=g&r=r&b=b&l=o&t=w&u=y&cube=g,g,g,g,g,g,g,g,g,r,r,r,r,r,r,r,r,r,b,b,b,b,b,b,b,b,b,o,o,o,o,o,o,o,o,o,w,w,w,w,w,w,w,w,w,y,y,y,y,y,y,y,y,y&face=R"
+        expected = ['g','g','w','g','g','w','g','g','w','r','r','r','r','r','r','r','r','r','y','b','b','y','b','b','y','b','b','o','o','o','o','o','o','o','o','o','w','w','b','w','w','b','w','w','b','y','y','g','y','y','g','y','y','g']       
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertEquals('rotated',resultDict['status'])
+        self.assertEquals(expected, resultDict['cube'])
         
